4bf67bb2d78fd35800e8fb37715a42affd9fe676
jMilic <Jura.Milic@avl.com>
2021-05-13 13:13:38 +0200

    Moved logic from main program to separate pokemon-encounter-api component.
    Merged "id" and "name" flags into a single "pokemon" flag of type string.
    
    Renamed data models and their fields. Fields now use struct tags for json specification.
    
    Refactored url concatenation to use path.Join method.
    
    Removed unnecessary documentation in fetchFromUrl method.

--- homework_1/Task-03/main.go
+++ homework_1/Task-03/main.go
@@ -1,117 +1,30 @@
-// package containing main executable for fetching Pokemon information
 package main
 
 import (
+	pokemonEncounterApi "code-cadets-2021/homework_1/Task-03/pokemon-encounter-api"
 	"encoding/json"
 	"flag"
 	"github.com/pkg/errors"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"strconv"
 )
 
-// url contains base path to pokeAPI
-const url = "https://pokeapi.co/api/v2/pokemon/"
-
-// pokemonDto contains fields which need to be in output
-type pokemonDto struct {
-	Name       string
-	Encounters []string
-}
-
-// pokemon is a basic model of Pokemon information
-// it is defined by json response for "https://pokeapi.co/api/v2/pokemon/1"
-type pokemon struct {
-	Name                     string
-	Location_area_encounters string
-}
-
-// areaEncounter is a basic model containing information about where a Pokemon can be found
-// it is defined by json response for "https://pokeapi.co/api/v2/pokemon/1/encounters"
-type areaEncounter struct {
-	Location_area struct {
-		Name string
-	}
-}
-
-// fetchFromUrl fetches data from given url.
-// returns data as an array of bytes and an error if it occurred
-func fetchFromUrl(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	bodyContent, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return bodyContent, nil
-}
-
 // main is the entrypoint for getting Pokemon information.
-// Valid flags are "id" of type int and "name" of type string
 func main() {
-	var id int
-	var name string
-
-	flag.IntVar(&id, "id", 0, "Id of Pokemon to be retrieved")
-	flag.StringVar(&name, "name", "wooper", "Name of Pokemon to be retrieved")
+	pokemon := flag.String("pokemon", "wooper", "Id or name of Pokemon to be retrieved")
 
 	flag.Parse()
 
-	var bodyContent []byte
-	var err error
-
-	if id == 0 {
-		bodyContent, err = fetchFromUrl(url + name)
-		if err != nil {
-			log.Fatal(err, "Error while fetching content")
-		}
-
-	} else {
-		bodyContent, err = fetchFromUrl(url + strconv.Itoa(id))
-		if err != nil {
-			log.Fatal(err, "Error while fetching content")
-		}
-	}
-
-	var decodedPokemon pokemon
-	err = json.Unmarshal(bodyContent, &decodedPokemon)
+	encounters, err := pokemonEncounterApi.GetEncounters(*pokemon)
 	if err != nil {
 		log.Fatal(
-			errors.WithMessage(err, "unmarshalling the JSON body content"),
+			errors.WithMessage(err, "error while getting encounters"),
 		)
 	}
 
-	bodyContent, err = fetchFromUrl(decodedPokemon.Location_area_encounters)
+	output, err := json.Marshal(encounters)
 	if err != nil {
 		log.Fatal(
-			errors.WithMessage(err, "Error while fetching content"),
-		)
-	}
-
-	var decodedEncounters []areaEncounter
-	err = json.Unmarshal(bodyContent, &decodedEncounters)
-	if err != nil {
-		log.Fatal(
-			errors.WithMessage(err, "unmarshalling the JSON body content"),
-		)
-	}
-
-	dto := pokemonDto{
-		Name: decodedPokemon.Name,
-	}
-	for _, val := range decodedEncounters {
-		dto.Encounters = append(dto.Encounters, val.Location_area.Name)
-	}
-
-	output, err := json.Marshal(dto)
-	if err != nil {
-		log.Fatal(
-			errors.WithMessage(err, "marshalling the JSON body content"),
+			errors.WithMessage(err, "error while marshaling pokemon encounters"),
 		)
 	}
 
---
+++ homework_1/Task-03/pokemon-encounter-api/pokemon-encounter-api.go
@@ -0,0 +1,112 @@
+package pokemon_encounter_api
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+// pokeApiUrl contains base path to pokeAPI
+const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+// pokemonData is a basic model of Pokemon information
+// defined by json response for "https://pokeapi.co/api/v2/pokemon/1"
+type pokemonData struct {
+	Name          string `json:"name"`
+	EncountersUrl string `json:"location_area_encounters"`
+}
+
+// PokemonEncounters contains a pokemon's name and where it can be encountered
+type PokemonEncounters struct {
+	Name       string
+	Encounters []string
+}
+
+// encounterData is a basic model containing information about where a Pokemon can be found
+// defined by json response for "https://pokeapi.co/api/v2/pokemon/1/encounters"
+type encounterData struct {
+	LocationArea struct {
+		Name string `json:"name"`
+	} `json:"location_area"`
+}
+
+// fetchFromUrl fetches data from given url
+func fetchFromUrl(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyContent, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bodyContent, nil
+}
+
+// fetchEncounters fetches encounterData from given url
+func fetchEncounters(url string) ([]encounterData, error) {
+	bodyContent, err := fetchFromUrl(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var decodedEncounters []encounterData
+	err = json.Unmarshal(bodyContent, &decodedEncounters)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodedEncounters, nil
+}
+
+// fetchPokemon fetches pokemonData data from given url
+func fetchPokemon(url string) (pokemonData, error) {
+	bodyContent, err := fetchFromUrl(url)
+	if err != nil {
+		return pokemonData{}, err
+	}
+
+	var decodedPokemon pokemonData
+	err = json.Unmarshal(bodyContent, &decodedPokemon)
+	if err != nil {
+		return pokemonData{}, err
+	}
+
+	return decodedPokemon, nil
+}
+
+// GetEncounters returns encounters for given pokemon
+func GetEncounters(pokemon string) (PokemonEncounters, error) {
+	u, err := url.Parse(pokeApiUrl)
+	if err != nil {
+		return PokemonEncounters{}, err
+	}
+
+	u.Path = path.Join(u.Path, pokemon)
+	completeUrl := u.String()
+
+	fetchedPokemon, err := fetchPokemon(completeUrl)
+	if err != nil {
+		return PokemonEncounters{}, err
+	}
+
+	completeUrl = fetchedPokemon.EncountersUrl
+
+	fetchedEncounters, err := fetchEncounters(completeUrl)
+	if err != nil {
+		return PokemonEncounters{}, err
+	}
+
+	output := PokemonEncounters{
+		Name: fetchedPokemon.Name,
+	}
+	for _, encounter := range fetchedEncounters {
+		output.Encounters = append(output.Encounters, encounter.LocationArea.Name)
+	}
+
+	return output, nil
+}
